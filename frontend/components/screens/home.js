0b757452a2d3b87a8c8c9b1b69434e030ae117cb
Yoon <leeon592@gmail.com>
2024-07-24 13:59:21 -0700

    Initial commit

---
+++ frontend/components/screens/home.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Context } from "../globalContext/globalContext.js"
+import containers from "../styles/containers.js"
+import fonts from "../styles/fonts.js"
+import buttons from "../styles/buttons.js"
+
+
+function Home({ navigation, route, props }){
+
+  const globalContext = useContext(Context)
+  const { isLoggedIn, appSettings } = globalContext;
+
+  return(
+    <View style={containers(appSettings).outerPage}>
+      <Text style={fonts(appSettings).h1}>Hello User !</Text>
+      <Text style={fonts(appSettings).h1}>YOU ARE HOME</Text>
+      <Text style={fonts(appSettings).p}>You are {(isLoggedIn)? '' : "Not "}logged in</Text>
+    </View>
+  )
+
+
+
+}
+
+export default Home;
---
+++ frontend/components/screens/landing.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Context } from "../globalContext/globalContext.js";
+import containers from "../styles/containers.js";
+import fonts from "../styles/fonts.js";
+import buttons from "../styles/buttons.js";
+import margins from "../styles/margins.js";
+
+function Landing({ navigation }) {
+  const globalContext = useContext(Context);
+  const { isLoggedIn, appSettings } = globalContext;
+
+  return (
+    <View style={containers(appSettings).outerPage}>
+      <Text style={fonts(appSettings).h1}>Hello User !</Text>
+      <Text style={fonts(appSettings).p}>You are {isLoggedIn ? '' : 'Not '}logged in</Text>
+      <TouchableOpacity
+        style={buttons(appSettings).landingpage}
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text>Sign In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[buttons(appSettings).landingpage, margins.topTenPercent]}
+        onPress={() => navigation.navigate('Register')}
+      >
+        <Text>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default Landing;
